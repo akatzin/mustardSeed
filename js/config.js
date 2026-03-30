776ad8f2531374c4a5aef2bc652c5d300f41c632
akatzin <akatzin@users.noreply.github.com>
2026-03-29 19:22:01 -0600

    chore: enable GitHub Pages by exposing secure public config

---
+++ js/config.js
@@ -0,0 +1,6 @@
+// Copia este archivo como config.js y reemplaza los valores
+const CONFIG = {
+    SUPABASE_URL: 'https://kftexgvlbvzrnsgciqyj.supabase.co',
+    SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_L6BrnaFUxyywadNLiJATCQ_LH-4DK9b'
+};
+
